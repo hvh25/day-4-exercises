a4cca61fe43d9099a0f5b53ea3a53617711d1460
hvh25 <hvh25@drexel.edu>
2014-07-24 16:38:24 -0400

    Update population.php

--- population.php
+++ population.php
@@ -1,6 +1,6 @@
 <?php
 $connection = mysql_connect('127.0.0.1', 'root', '');
-mysql_select_db('auroratesting');
+mysql_select_db('population');
 
 // mysql_connect(‘SQLdata.com’, ‘Jones’, ‘un1c0rn’, ‘my_database’);
 // Check connection
