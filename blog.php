8826dab1238a92881bd9e909e89e686b5c070def
hvh25 <hvh25@drexel.edu>
2014-07-24 16:11:04 -0400

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,41 +1,26 @@
 <h1>Welcome to the Cupcake Blog!</h1>
  
+ 
 <?php
+$connection = mysql_connect('127.0.0.1', 'root', '');
+mysql_select_db('day-4-exercises');
+
+// mysql_connect(‘SQLdata.com’, ‘Jones’, ‘un1c0rn’, ‘my_database’);
+// Check connection
+if (!$connection) {
+  die('Unable to connect: ' . mysql_errno());
+}
  
-$articles = array();
- 
-$article = new stdClass();
-$article->name = 'Frosting Adventures';
-$article->author = 'Mr.Frosting';
-$article->body = 'Applicake jujubes tart toffee unerdwear.com gummies powder
-jelly beans toffee. Donut brownie marzipan toffee liquorice. Cupcake bear
-claw dragée unerdwear.com sugar plum.';
-$articles[] = $article;
- 
- 
-$article = new stdClass();
-$article->name = 'Sprinkles Craze';
-$article->author = 'Missy Sprinkles';
-$article->body = 'Tootsie roll muffin chocolate cake sugar plum marzipan
-candy cake jelly-o macaroon. Toffee gingerbread carrot cake macaroon
-brownie. Gummi bears gummi bears chocolate sugar plum unerdwear.com cookie
-carrot cake lollipop gummies. Pudding danish chocolate cake marzipan carrot
-cake pudding sugar plum cake gingerbread.';
-$articles[] = $article;
- 
- 
-$article = new stdClass();
-$article->name = 'What\s Your Flavor?';
-$article->author = 'Vanilla Ice';
-$article->body = 'Candy canes caramels fruitcake croissant macaroon carrot
-cake. Bear claw chocolate cake oat cake powder soufflé wafer candy pastry
-dessert. Gummies lollipop pie jujubes gingerbread tiramisu applicake.';
-$articles[] = $article;
- 
-foreach ($articles as $piece){
-    print '<h2>' . $piece->name . '</h2>';
-    print '<em>By: ' . $piece->author . '</em><br/>';
-    print $piece->body . '<br/><br/>';
+if ($connection) {
+  $result = mysql_query('SELECT name, author, body FROM blog');
+  
+    // Fetch the data from the result
+  while ($row = mysql_fetch_array($result)) {
+    echo "Name: ". $row['name']."<br>".
+         "Author: ".$row['author']."<br>".
+         "Body: ".$row['bod'].'<br>"'.
+  }
+?>
  
 }
  
