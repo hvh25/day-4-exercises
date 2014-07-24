0bae55a2a455dcdd61a2831b4be85af8a7e1f50c
hvh25 <hvh25@drexel.edu>
2014-07-24 15:54:30 -0400

    Update population.php

--- population.php
+++ population.php
@@ -1,26 +1,14 @@
 <?php
-$city = $_GET['city'];
-?>
-<h1>Welcome to Population Data Online</h1>
-<?
-$cities = array(
-  'Philadelphia' => 1548000,
-  'New York' => 8337000,
-  'Los Angeles' => 3858000,
-  'Seattle' => 634535,
-  'Boston' => 636479,
-);
-if ($city && isset($cities[$city])) {
-  print '<p>The population of <strong>' . $city . '</strong> is <strong>' . $cities[$city] . '</strong></p>';
+// Check connection
+if (!$connection) {
+  die('Unable to connect: ' . mysql_errno());
 }
  
-?>
-<h2>Our Cities</h2>
-<ul>
-<?php
-  foreach ($cities as $name => $value) {
-    print '<li><a href="/jody/population.php?city=' . $name  . '">' . $name
-    . '</a></li>';
+if ($connection) {
+  $result = mysql_query('SELECT city_name, population FROM day-4-exercises');
+  
+    // Fetch the data from the result
+  while ($row = mysql_fetch_array($result)) {
+    echo "The population of ". $row['city_name']."is". $row['population']
   }
 ?>
-</ul>
